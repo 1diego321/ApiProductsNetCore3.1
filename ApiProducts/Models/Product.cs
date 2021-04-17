2e0cc67565fc26563014227696a7e7b8edbb3439
Luis Diego Solis Camacho <1diego321@gmail.com>
2021-04-17 16:48:44 -0600

    productmodel

---
+++ ApiProducts/Models/Product.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiProducts.Models
+{
+    public class Product
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Code { get; set; }
+
+        public string AlternativeCode { get; set; }
+
+        public string Description { get; set; }
+
+        public decimal Price { get; set; }
+
+        public decimal Stock { get; set; }
+    }
+}
